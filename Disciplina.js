76dfdcf08acc35c566378360b6acd3dbb4699dc8
Andrade <igor.andrade@capgemini.com>
2020-12-08 11:29:38 -0300

    organização de pastas para  o projeto

--- Disciplina.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Disciplina = void 0;
-var Disciplina = /** @class */ (function () {
-    function Disciplina() {
-    }
-    return Disciplina;
-}());
-exports.Disciplina = Disciplina;
--- Disciplina.ts
+++
@@ -1,3 +0,0 @@
-export class Disciplina{
-    professor: string;
-}

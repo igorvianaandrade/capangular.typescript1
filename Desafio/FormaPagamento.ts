bcaf9b5674554d13c39bb592385362429f05f159
Andrade <igor.andrade@capgemini.com>
2020-12-08 08:01:15 -0300

    bug1

--- Desafio/FormaPagamento.ts
+++ Desafio/FormaPagamento.ts
@@ -7,18 +7,18 @@ export class FormaPagamentoClass{
     public pagarPorBoletoPorPadrao():string {
         this.formaPagamento.pagamento = "boleto";
 
-        return `Pagamento via ${this.pagamento} ...`;
+        return `Pagamento via ${this.formaPagamento.pagamento} ...`;
     }
 
     public pagarPorCartaoCredito():string{
         this.formaPagamento.pagamento = "cartão de crédito";
 
-        return `Pagamento via ${this.pagamento} ...`;
+        return `Pagamento via ${this.formaPagamento.pagamento} ...`;
     }
 
     public pagarPorCartaoDebito():string{
         this.formaPagamento.pagamento = "cartão de débito";
-        return `Pagamento via ${this.pagamento} ...`;
+        return `Pagamento via ${this.formaPagamento.pagamento} ...`;
     }
 }
 
